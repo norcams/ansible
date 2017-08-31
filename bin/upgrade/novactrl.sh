bfc2ec4d1fe953bf1abc9916554ae443b7c36ce3
Anders Vaage <anders@unix.uib.no>
2017-08-31 12:10:31 +0200

    ansible option (i.e. --check) as second arg in novactrl.sh

--- bin/upgrade/novactrl.sh
+++ bin/upgrade/novactrl.sh
@@ -3,19 +3,26 @@
 function usage {
   echo ""
   echo "This will upgrade novactrl in a location, including deps"
-  echo "./${0} <location>"
+  echo "./${0} <location> --<opt>"
+  echo "Example: ./${0} test01 --check"
   echo ""
   exit 1
 }
 
-if [ $# -ne 1 ]; then
+if [ $# -lt 1 ]; then
   usage
 fi
 
 loc=$1
 
-sudo ansible-playbook -e "hosts=${loc}-identity-01" lib/puppetrun.yaml
-sudo ansible "${loc}-proxy-01" --become -m shell -a '. /root/openrc && for id in `openstack endpoint list --service novav3 -f json | jq -r ".[] | .ID"`; do openstack endpoint delete $id; done'
-sudo ansible-playbook -e "hosts=${loc}-db-02" lib/puppetrun.yaml
-sudo ansible-playbook -e "hosts=${loc}-db-02" lib/puppetrun.yaml
-sudo ansible-playbook -e "hosts=${loc}-novactrl-01" lib/upgrade/nova.yaml
+if [ -z $2 ]; then
+  opt=""
+else
+  opt="$2"
+fi
+
+sudo ansible-playbook $opt -e "hosts=${loc}-identity-01" lib/puppetrun.yaml
+sudo ansible $opt "${loc}-proxy-01" --become -m shell -a '. /root/openrc && for id in `openstack endpoint list --service novav3 -f json | jq -r ".[] | .ID"`; do openstack endpoint delete $id; done'
+sudo ansible-playbook $opt -e "hosts=${loc}-db-02" lib/puppetrun.yaml
+sudo ansible-playbook $opt -e "hosts=${loc}-db-02" lib/puppetrun.yaml
+sudo ansible-playbook $opt -e "hosts=${loc}-novactrl-01" lib/upgrade/nova.yaml
