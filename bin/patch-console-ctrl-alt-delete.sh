81b6b50af5613368bbf31acc06b1d33bdedd4e70
mikaeld <mikael.dalsgard@usit.uio.no>
2017-09-20 14:57:36 +0200

    Add patches and ansible job adding a ctrl-add-delete button to the spice console

---
+++ bin/patch-console-ctrl-alt-delete.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+if [ ! -f "ansible.cfg" ]; then
+  echo "Run this from ansible root where the ansible git repo is checked out"
+  exit 1
+fi
+
+function usage {
+  echo ""
+  echo "This will patch the console server to include a ctrl-alt-delete-button"
+  echo "./${0} <location>"
+  echo ""
+  exit 1
+}
+
+if [ $# -ne 1 ]; then
+  usage
+fi
+
+loc=$1
+
+sudo ansible-playbook -e "hosts=${loc}-console-01 patchfile=${HOME}/ansible/files/patches/spice.css-ctrl-alt-delete-button.diff dest=/usr/share/spice-html5/spice.css" lib/patch.yaml
+sudo ansible-playbook -e "hosts=${loc}-console-01 patchfile=${HOME}/ansible/files/patches/spice_auto.html-ctrl-alt-delete-button.diff dest=/usr/share/spice-html5/spice_auto.html" lib/patch.yaml
