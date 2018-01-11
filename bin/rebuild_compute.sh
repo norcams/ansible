5df7a7fe25fa2e9616f19510d12f3a8fe19c365c
tanzr <tannaznr@mail.uio.no>
2018-01-11 13:04:03 +0100

    removed else

--- bin/rebuild_compute.sh
+++ bin/rebuild_compute.sh
@@ -16,31 +16,30 @@ if ! [[ ${1} =~ ^[A-Za-z0-9._]+[-]+["compute"]+[-]+[A-Za-z0-9._] ]]; then
   echo "./${0} <location-compute-number> --force"
   echo ""
   exit 1
-else
-  if [ $# -ne 2 ]; then
-    usage
-  fi
-
-  if [ "${2}" != '--force' ]; then
-    usage
-  fi
-  host=$1
-
-  # Use hostname not fqdn
-  if [[ $host == *.* ]] ; then
-    usage
-  fi
-
-  IFS='-' read -r -a hostname <<< "$host"
-  location=${hostname[0]}
-
-  sudo ansible-playbook -e "hosts=${location}-proxy-01 compute_host=${host}" lib/prepare_compute_reinstall.yaml
-  sudo ansible-playbook -e "hosts=${host}" lib/reboot.yaml
-  sudo ansible-playbook -e "hosts=${location}-login host=${host}" lib/ssh_host_keys.yaml
-  sleep 120
-  sudo ansible-playbook -e "hosts=${host}" lib/puppetrun.yaml
-  sudo ansible-playbook -e "hosts=${host}" lib/push_secrets.yaml
-  sudo ansible-playbook -e "hosts=${host}" lib/puppetrun.yaml
-  sudo ansible-playbook -e "hosts=${host} patchfile=${HOME}/ansible/files/patches/python-nova-newton-centos-7.3.0-discard.diff dest=/usr/lib/python2.7/site-packages/nova/virt/libvirt/driver.py" lib/patch.yaml
-  sudo ansible-playbook -e "hosts=${host} name=openstack-nova-compute.service" lib/systemd_restart.yaml
 fi
+if [ $# -ne 2 ]; then
+  usage
+fi
+
+if [ "${2}" != '--force' ]; then
+  usage
+fi
+host=$1
+
+# Use hostname not fqdn
+if [[ $host == *.* ]] ; then
+  usage
+fi
+
+IFS='-' read -r -a hostname <<< "$host"
+location=${hostname[0]}
+
+sudo ansible-playbook -e "hosts=${location}-proxy-01 compute_host=${host}" lib/prepare_compute_reinstall.yaml
+sudo ansible-playbook -e "hosts=${host}" lib/reboot.yaml
+sudo ansible-playbook -e "hosts=${location}-login host=${host}" lib/ssh_host_keys.yaml
+sleep 120
+sudo ansible-playbook -e "hosts=${host}" lib/puppetrun.yaml
+sudo ansible-playbook -e "hosts=${host}" lib/push_secrets.yaml
+sudo ansible-playbook -e "hosts=${host}" lib/puppetrun.yaml
+sudo ansible-playbook -e "hosts=${host} patchfile=${HOME}/ansible/files/patches/python-nova-newton-centos-7.3.0-discard.diff dest=/usr/lib/python2.7/site-packages/nova/virt/libvirt/driver.py" lib/patch.yaml
+sudo ansible-playbook -e "hosts=${host} name=openstack-nova-compute.service" lib/systemd_restart.yaml
