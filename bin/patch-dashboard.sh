e760ce7cef04a213c3823d564028745967207d0f
tanzr <tannazn@mail.uio.no>
2018-02-23 13:21:24 +0100

    added a patch script for dashboard

---
+++ bin/patch-dashboard.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# print help
+function usage {
+  echo ""
+  echo "Run this script to update the size error message in dashboard."
+  echo "bin/patch-dashboard.sh <location>-dashboard-01"
+  echo ""
+  exit 1
+}
+
+host=$1
+
+if [ $# -ne 1 ]; then
+  usage
+fi
+
+sudo ansible-playbook -e "hosts=${host} patchfile=${HOME}/usr/share/openstack-dashboard/openstack_dashboard/dashboards/project/instances/tables.py" lib/patch.yaml
+sudo ansible-playbook -e "hosts=${host} name=httpd" lib/systemd_restart.yaml
