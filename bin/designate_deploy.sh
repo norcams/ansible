348a9aab9fa44692e7da02ace3bf85f43aa2fc94
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2018-09-12 15:30:08 +0200

    upgrade designate_deploy playbook to queens

--- bin/designate_deploy.sh
+++ bin/designate_deploy.sh
@@ -18,5 +18,5 @@ fi
 echo "====> Running playbook: lib/designate_pool_update.yaml"
 ansible-playbook -e "myhosts=${LOC}-dns-01" lib/designate_pool_update.yaml
 
-echo "====> Running playbook: lib/designate_deploy.yaml"
-ansible-playbook -e "myhosts=${LOC}-identity,${LOC}-dns" lib/designate_deploy.yaml
+#echo "====> Running playbook: lib/designate_deploy.yaml"
+#ansible-playbook -e "myhosts=${LOC}-identity,${LOC}-dns" lib/designate_deploy.yaml
