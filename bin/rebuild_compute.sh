31eb2e9dadd65403f758a7bf459df7c27daaadbf
Anders Vaage <anders@unix.uib.no>
2018-12-20 13:42:11 +0100

    use node reinstall instead node rebuild for compute nodes in rebuild script

--- bin/rebuild_compute.sh
+++ bin/rebuild_compute.sh
@@ -35,9 +35,9 @@ fi
 echo
 
 sudo ansible-playbook -e "myhosts=${host}" lib/check_for_running_instances.yaml || exit 1
-sudo ansible-playbook -e "myhosts=${location}-proxy-01 install_host=${host}" lib/prepare_rebuild.yaml
+sudo ansible-playbook -e "myhosts=${location}-proxy-01 install_host=${host}" lib/reinstall.yaml
 sudo ansible-playbook -e "myhosts=${host}" lib/reboot.yaml
-sudo ansible-playbook -e "myhosts=${location}-login host=${host}" lib/ssh_host_keys.yaml
+#sudo ansible-playbook -e "myhosts=${location}-login host=${host}" lib/ssh_host_keys.yaml
 sleep 120
 sudo ansible-playbook -e "myhosts=${host}" lib/puppetrun.yaml
 sudo ansible-playbook -e "myhosts=${host}" lib/push_secrets.yaml
