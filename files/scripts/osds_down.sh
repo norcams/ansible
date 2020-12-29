c6bc804d70f11f98c1ae6995221d6a700385c0de
edpto <tor.ladre@it.uib.no>
2020-12-29 15:02:32 +0100

    repurpose to down

--- files/scripts/osds_down.sh
+++ files/scripts/osds_down.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-# Set all OSDs for a storage host to "out"
+# Set all OSDs for a storage host to "down"
 #
 for osd in `ceph-volume lvm list |grep ===== | cut -d' ' -f2 | cut -c 5-` ; do
   while ! /bin/ceph osd ok-to-stop $osd &>/dev/null ; do
-    echo "Not OK to set OSD $osd now. Waiting..."
+    echo "Not OK to stop OSD $osd now. Waiting..."
     sleep 30
   done
-  echo "OK to stop OSD $osd now. Stopping..."
-  /bin/ceph osd out $osd
+  echo "OK to stop OSD $osd now. Set OSD down..."
+  /bin/ceph osd down $osd
   /bin/sleep 5
 #  echo "$osd"
 done
