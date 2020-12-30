4f242b2f5dcef543139b8a8ca4296ac1c1635a16
edpto <tor.ladre@it.uib.no>
2020-12-30 20:42:37 +0100

    disable the osds as well

--- files/scripts/osds_down.sh
+++ files/scripts/osds_down.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Stop all OSDs on storage host
+# Stop all OSDs on storage host and disable them
 #
 for osd in `ceph-volume lvm list |grep ===== | cut -d' ' -f2 | cut -c 5-` ; do
   while ! /bin/ceph osd ok-to-stop $osd &>/dev/null ; do
@@ -9,6 +9,7 @@ for osd in `ceph-volume lvm list |grep ===== | cut -d' ' -f2 | cut -c 5-` ; do
   done
   echo "OK to stop OSD $osd now. Set OSD down..."
   /bin/systemctl stop ceph-osd@$osd.service
+  /bin/systemctl disable ceph-osd@$osd.service
   /bin/sleep 5
 #  echo "$osd"
 done
