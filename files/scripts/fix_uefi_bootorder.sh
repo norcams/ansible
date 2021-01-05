db1d998945ab0e55910715f0eb10c8caa2730176
edpto <tor.ladre@it.uib.no>
2021-01-05 17:34:02 +0100

    always exit with code 0

--- files/scripts/fix_uefi_bootorder.sh
+++ files/scripts/fix_uefi_bootorder.sh
@@ -4,8 +4,8 @@
 # Set PXE device as first boot device, CentOS as number two.
 #
 servermodel=$( /sbin/dmidecode |grep ModelName )
-[[ -f /sbin/efibootmgr  ]] || { echo "This server does not have UEFI boot enabled"; exit 1; }
-[[ $servermodel =~ .(FC|[RTM])[1-9][4]. ]] || { echo "This is not a Dell 14G Server"; exit 2; }
+[[ -f /sbin/efibootmgr  ]] || { echo "This server does not have UEFI boot enabled"; exit 0; }
+[[ $servermodel =~ .(FC|[RTM])[1-9][4]. ]] || { echo "This is not a Dell 14G Server"; exit 0; }
 pxedevice=$(/sbin/efibootmgr | grep -i "integrated NIC" | awk '{ print $1 }' | sed 's/[^0-9]*//g')
 osdevice=$(/sbin/efibootmgr | grep -i "centos" | awk '{ print $1 }' | sed 's/[^0-9]*//g')
 /sbin/efibootmgr --bootorder $pxedevice,$osdevice
