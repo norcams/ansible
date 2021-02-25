3c3c729fee515726c245a9a922f8aee1d563d44b
tanzr <tanzr@users.noreply.github.com>
2021-02-25 07:22:53 +0100

    added a patch to hide create volume

--- bin/patch-dashboard.sh
+++ bin/patch-dashboard.sh
@@ -4,6 +4,7 @@
 function usage {
   echo ""
   echo "Run this script to update the size error message in dashboard."
+  echo "And to hide create volume in the workflow for sources in dashboard."
   echo "bin/patch-dashboard.sh <location>-dashboard-01"
   echo ""
   exit 1
@@ -16,4 +17,5 @@ if [ $# -ne 1 ]; then
 fi
 
 sudo ansible-playbook -e "myhosts=${host} patchfile=${HOME}/ansible/files/patches/horizon-tables-size.diff dest=/usr/share/openstack-dashboard/openstack_dashboard/dashboards/project/instances/tables.py" lib/patch.yaml
+sudo ansible-playbook -e "myhosts=${host} patchfile=${HOME}/ansible/files/patches/horizon-hide-create-volume-bootsource.diff dest=/usr/share/openstack-dashboard/openstack_dashboard/dashboards/project/static/dashboard/project/workflow/launch-instance/launch-instance-model.service.js" lib/patch.yaml
 sudo ansible-playbook -e "myhosts=${host} name=httpd" lib/systemd_restart.yaml
