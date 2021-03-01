3a02c1dce4021be7b5924d0614214ffda6927289
edpto <tor.ladre@it.uib.no>
2021-03-01 11:54:46 +0100

    expand uefi boot fix

--- files/scripts/fix_uefi_bootorder.sh
+++ files/scripts/fix_uefi_bootorder.sh
@@ -3,10 +3,12 @@
 # Workaround for Dell 14G Servers' boot order reset "feature" after OS install
 # Set PXE device as first boot device, CentOS as number two.
 #
+#!/bin/bash
 servermodel=$( /sbin/dmidecode |grep ModelName )
 [[ -f /sbin/efibootmgr  ]] || { echo "This server does not have UEFI boot enabled"; exit 0; }
-[[ $servermodel =~ .(FC|[RTM])[1-9][4]. ]] || { echo "This is not a Dell 14G Server"; exit 0; }
-pxedevice=$(/sbin/efibootmgr | grep -i "integrated NIC" | awk '{ print $1 }' | sed 's/[^0-9]*//g')
+[[ $servermodel =~ .(FC|[RTM])[1-9][4-5]. ]] || { echo "This is not a Dell 14G Server"; exit 0; }
+if [[ ($( echo $servermodel | grep "R740" )) ]]; then niccard=Integrated; else niccard=Embedded; fi
+pxedevice=$(/sbin/efibootmgr | grep -i "$niccard NIC" | awk '{ print $1 }' | sed 's/[^0-9]*//g')
 osdevice=$(/sbin/efibootmgr | grep -i "centos" | awk '{ print $1 }' | sed 's/[^0-9]*//g')
 /sbin/efibootmgr --bootorder $pxedevice,$osdevice
 exit 0
