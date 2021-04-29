68e24b311cac3a24ebed15637c973ddd272e8b91
TorLdre <tor.ledre@gmail.com>
2021-04-29 10:36:11 +0200

    fix ownership

--- files/scripts/repair_bluestore.sh
+++ files/scripts/repair_bluestore.sh
@@ -4,4 +4,6 @@ for osd in `ceph-volume lvm list |grep ===== | cut -d' ' -f2 | cut -c 5-` ; do
   /bin/systemctl stop ceph-osd@$osd.service
   /bin/sleep 2
   /bin/ceph-bluestore-tool repair --path /var/lib/ceph/osd/ceph-$osd
+  /bin/chown ceph:ceph /var/lib/ceph/osd/ceph-$osd/block.db
+  /bin/chown -h ceph:ceph /var/lib/ceph/osd/ceph-$osd/block.db
 done
