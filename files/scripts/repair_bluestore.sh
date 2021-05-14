36b01339d6e4c9cf9fa5cf541659e2876ee6fb97
TorLdre <tor.ledre@gmail.com>
2021-05-14 16:33:01 +0200

    start osd after repair

--- files/scripts/repair_bluestore.sh
+++ files/scripts/repair_bluestore.sh
@@ -6,4 +6,6 @@ for osd in `ceph-volume lvm list |grep ===== | cut -d' ' -f2 | cut -c 5-` ; do
   /bin/ceph-bluestore-tool repair --path /var/lib/ceph/osd/ceph-$osd
   /bin/chown ceph:ceph /var/lib/ceph/osd/ceph-$osd/block.db
   /bin/chown -h ceph:ceph /var/lib/ceph/osd/ceph-$osd/block.db
+  /bin/sleep 5
+  /bin/systemctl start ceph-osd@$osd.service
 done
