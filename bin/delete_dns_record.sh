73872a215d6bfabbe016b81d1e21d533dee8a718
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2021-12-10 15:36:18 +0100

    extra example

--- bin/delete_dns_record.sh
+++ bin/delete_dns_record.sh
@@ -13,8 +13,9 @@ function usage {
   echo '  type - The DNS record type, e.g. "A"'
   echo '  name - The name of the record'
   echo
-  echo 'Example:'
+  echo 'Examples:'
   echo '  bin/delete_dns_record.sh test01 A test01-db-01.mgmt.test01.uhdc.no'
+  echo '  bin/delete_dns_record.sh osl PTR 43.63.37.158.in-addr.arpa'
   echo
   exit 1
 }
