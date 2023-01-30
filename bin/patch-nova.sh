fa913d8c5e58422530dd01c8b84fd58b4170819d
furenest <107837528+furenest@users.noreply.github.com>
2023-01-30 15:37:41 +0100

    fix: recompile pyc-files after patch

--- bin/patch-nova.sh
+++ bin/patch-nova.sh
@@ -4,7 +4,7 @@
 function usage {
   echo ""
   echo "Run this script to patch nova for CVE-2022-47951"
-  echo "bin/patch-nova.sh <location>-novactrlcompute-01"
+  echo "bin/patch-nova.sh <location>-compute-01"
   echo ""
   exit 1
 }
@@ -16,4 +16,10 @@ if [ $# -ne 1 ]; then
 fi
 
 sudo ansible-playbook -e "myhosts=${host} patchfile=../files/patches/nova-fix-CVE-2022-47951.diff basedir=/usr/lib/python3.6/site-packages" lib/patch.yaml
-sudo ansible-playbook -e "myhosts=${host} name=openstack-nova-api" lib/systemd_restart.yaml
+
+sudo ansible ${host} -m file -a "name=/usr/lib/python3.6/site-packages/nova/conf/__pycache__/compute.cpython-36.pyc state=absent"
+sudo ansible ${host} -m file -a "name=/usr/lib/python3.6/site-packages/nova/virt/__pycache__/images.cpython-36.pyc state=absent"
+sudo ansible ${host} -a "python3 -m compileall /usr/lib/python3.6/site-packages/nova/virt/"
+sudo ansible ${host} -a "python3 -m compileall /usr/lib/python3.6/site-packages/nova/conf/"
+
+sudo ansible-playbook -e "myhosts=${host}" lib/restart_compute_services.yaml
