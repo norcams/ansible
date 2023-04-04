958d0d7e5905426b95b66ba669153760debaf8a2
furenest <terje.furenes@uib.no>
2023-04-04 15:02:27 +0200

    feat: add nova cve-2022-47951 fix to rebuild_compute.sh

--- bin/patch-nova.sh
+++ bin/patch-nova.sh
@@ -15,14 +15,7 @@ if [ $# -ne 1 ]; then
   usage
 fi
 
-sudo ansible-playbook -e "myhosts=${host} patchfile=../files/patches/nova-fix-CVE-2022-47951.diff basedir=/usr/lib/python3.6/site-packages" lib/patch.yaml
-
-sudo ansible ${host} -o -m file -a "name=/usr/lib/python3.6/site-packages/nova/conf/__pycache__/compute.cpython-36.pyc state=absent"
-sudo ansible ${host} -o -m file -a "name=/usr/lib/python3.6/site-packages/nova/conf/__pycache__/compute.cpython-36.opt-1.pyc state=absent"
-sudo ansible ${host} -o -m file -a "name=/usr/lib/python3.6/site-packages/nova/virt/__pycache__/images.cpython-36.pyc state=absent"
-sudo ansible ${host} -o -m file -a "name=/usr/lib/python3.6/site-packages/nova/virt/__pycache__/images.cpython-36.opt-1.pyc state=absent"
-sudo ansible ${host} -o -a "python3 -m compileall /usr/lib/python3.6/site-packages/nova/virt/"
-sudo ansible ${host} -o -a "python3 -m compileall /usr/lib/python3.6/site-packages/nova/conf/"
+sudo ansible-playbook -e "myhosts=${host}" lib/patch_nova_cve_2022_47951.yaml
 
 sudo ansible ${host} -o -m systemd -a "name=openstack-nova-compute state=restarted"
 sleep 20
