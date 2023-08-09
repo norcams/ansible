67e2580e868ab212d3a2de85fb3bc5fc833d249e
edpto <tor.ladre@it.uib.no>
2023-08-09 10:16:46 +0200

    fix script for select 13G PE servers

--- files/scripts/fix_uefi_bootorder.sh
+++ files/scripts/fix_uefi_bootorder.sh
@@ -6,10 +6,11 @@
 #!/bin/bash
 servermodel=$( /sbin/dmidecode |grep ModelName )
 [[ -f /sbin/efibootmgr  ]] || { echo "This server does not have UEFI boot enabled"; exit 0; }
-[[ $servermodel =~ .(FC|[RTM])[1-9][4-5]. ]] || { echo "This is not a Dell 14G Server"; exit 0; }
-if [[ ($( echo $servermodel | grep "R640" )) ]]; then niccard=Integrated; else niccard=Embedded; fi
-if [[ ($( echo $servermodel | grep "R740" )) ]]; then niccard=Integrated; else niccard=Embedded; fi
+[[ $servermodel =~ .(FC|[RTM])[1-9][3-5]. ]] || { echo "This is not a Dell 13G or 14G Server"; exit 0; }
+if [[ ($( echo $servermodel | grep "R630" )) ||\
+($( echo $servermodel | grep "R640" )) ||\
+($( echo $servermodel | grep "R740" ))  ]]; then niccard=Integrated; else niccard=Embedded; fi
 pxedevice=$(/sbin/efibootmgr | grep -i "$niccard NIC" | awk '{ print $1 }' | sed 's/[^0-9]*//g')
-osdevice=$(/sbin/efibootmgr | grep -i "centos" | awk '{ print $1 }' | sed 's/[^0-9]*//g')
+osdevice=$(/sbin/efibootmgr | grep -i "almalinux" | awk '{ print $1 }' | sed 's/[^0-9]*//g')
 /sbin/efibootmgr --bootorder $pxedevice,$osdevice
 exit 0
