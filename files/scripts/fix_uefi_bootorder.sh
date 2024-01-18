76ba6827e97a2ad9d7de4e61c4a071947f796bf0
Trond Hasle Amundsen <trondham@uio.no>
2024-01-18 14:26:52 +0100

    revamp fix_uefi_bootorder.sh

--- files/scripts/fix_uefi_bootorder.sh
+++ files/scripts/fix_uefi_bootorder.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 #
 # Workaround for Dell 14G Servers' boot order reset "feature" after OS install
-# Set PXE device as first boot device, CentOS as number two.
+# Set PXE device as first boot device, AlmaLinux as number two.
 #
-#!/bin/bash
-servermodel=$( /sbin/dmidecode |grep ModelName )
-[[ -f /sbin/efibootmgr  ]] || { echo "This server does not have UEFI boot enabled"; exit 0; }
-[[ $servermodel =~ .(FC|[RTM])[1-9][3-5]. ]] || { echo "This is not a Dell 13G or 14G Server"; exit 0; }
-if [[ ($( echo $servermodel | grep "R630" )) ||\
-($( echo $servermodel | grep "R640" )) ||\
-($( echo $servermodel | grep "R740" ))  ]]; then niccard=Integrated; else niccard=Embedded; fi
-pxedevice=$(/sbin/efibootmgr | grep -i "$niccard NIC" | awk '{ print $1 }' | sed 's/[^0-9]*//g')
-osdevice=$(/sbin/efibootmgr | grep -i "almalinux" | awk '{ print $1 }' | sed 's/[^0-9]*//g')
-/sbin/efibootmgr --bootorder $pxedevice,$osdevice
+
+# Set proper PATH
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+export PATH
+
+# Exit if not UEFI boot
+if [ ! -f /sbin/efibootmgr  ]; then
+    echo "This server does not have UEFI boot enabled"
+    exit 0
+fi
+
+# Get server model name
+servermodel=$(dmidecode -s system-product-name)
+
+# Exit if not Dell PowerEdge 13th gen or newer
+if [[ ! $servermodel =~ .(FC|[RTM])[1-9][3-5]. ]]; then
+    echo "This is not a Dell 13G / 14G / 15G Server"
+    exit 0
+fi
+
+# Remove duplicate OS entries (from previous reinstalls)
+entries_to_delete=()
+for entry in $(efibootmgr | awk '/AlmaLinux/ {print $1}' | sed 's/[^0-9]*//g'); do
+    entries_to_delete+=( $entry )
+done
+unset entries_to_delete[-1]
+for entry in ${entries_to_delete[@]}; do
+    efibootmgr --bootnum $entry --delete-bootnum
+done
+
+# Get efi boot numbers
+pxe_number=$(efibootmgr | awk '/(Integrated|Embedded) NIC/ {print $1}' | sed 's/[^0-9]*//g')
+os_number=$(efibootmgr | awk '/AlmaLinux/ {print $1}' | sed 's/[^0-9]*//g')
+
+# Set correct boot order
+efibootmgr --bootorder $pxe_number,$os_number
+
+# Exit gracefully
 exit 0
