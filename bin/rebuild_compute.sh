ce5d75e62448a6f3c6c677ca6f950a093a1fdea5
furenest <terje.furenes@uib.no>
2024-06-14 16:32:57 +0200

    chore: remove ussuri-patch

--- bin/rebuild_compute.sh
+++ bin/rebuild_compute.sh
@@ -49,7 +49,6 @@ sudo ansible-playbook -e "myhosts=${host}" lib/puppetrun.yaml
 # FIXME: ip6tables issue must be solved in puppet code
 #sudo ansible -u iaas -a 'ip6tables -I INPUT 10 -p udp -m multiport --dports 3784,3785,4784,4785 -m comment --comment "912 bird allow bfd ipv6" -m state --state NEW -j ACCEPT' -m shell ${host}
 #sudo ansible-playbook -e "myhosts=${host} patchfile=${HOME}/ansible/files/patches/python-nova-newton-centos-7.3.0-discard.diff dest=/usr/lib/python2.7/site-packages/nova/virt/libvirt/driver.py" lib/patch.yaml
-sudo ansible-playbook -e "myhosts=${host}" lib/patch_nova_cve_2022_47951.yaml
 sudo ansible-playbook -e "myhosts=${host} name=openstack-nova-compute.service" lib/systemd_restart.yaml
 sudo ansible-playbook -e "myhosts=${host} name=openstack-nova-metadata-api.service" lib/systemd_restart.yaml
 sudo ansible-playbook -e "myhosts=${host} name=calico-felix" lib/systemd_restart.yaml
