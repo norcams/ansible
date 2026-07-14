7e0897f69286adcf4583718367550014b4d1131f
Raymond Kristiansen <raymond.kristiansen@uib.no>
2026-07-14 08:59:35 +0200

    feat: update uefi bootorder script for Dell 17G

--- files/scripts/fix_uefi_bootorder.sh
+++ files/scripts/fix_uefi_bootorder.sh
@@ -18,10 +18,20 @@ fi
 servermodel=$(dmidecode -s system-product-name)
 
 # Exit if not Dell PowerEdge 13th gen or newer
-if [[ ! $servermodel =~ .(FC|[RTM])[1-9][3-6]. ]]; then
-    echo "This is not a Dell 13G / 14G / 15G / R16 Server"
+if [[ ! $servermodel =~ .(FC|[RTM])[1-9][3-7]. ]]; then
+    echo "This is not a Dell 13G / 14G / 15G / 16G / 17G Server"
     exit 0
 fi
+
+# Remove duplicate NIC entries (from previous reinstalls)
+entries_to_delete=()
+for entry in $(efibootmgr | awk '/NIC in Slot / {print $1}' | sed 's/[^0-9]*//g'); do
+    entries_to_delete+=( $entry )
+done
+unset entries_to_delete[-1]
+for entry in ${entries_to_delete[@]}; do
+    efibootmgr --bootnum $entry --delete-bootnum
+done
 
 # Remove duplicate OS entries (from previous reinstalls)
 entries_to_delete=()
@@ -34,6 +44,7 @@ for entry in ${entries_to_delete[@]}; do
 done
 
 # Get efi boot numbers
+pxe_number=$(efibootmgr | awk '/(Integrated NIC|Embedded NIC|NIC in Slot) / {print $1}' | sed 's/[^0-9]*//g')
 pxe_number=$(efibootmgr | awk '/(Integrated|Embedded) NIC/ {print $1}' | sed 's/[^0-9]*//g')
 os_number=$(efibootmgr | awk '/AlmaLinux/ {print $1}' | sed 's/[^0-9]*//g')
 
